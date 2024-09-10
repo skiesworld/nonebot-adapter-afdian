22bc9f5eb5e2a99bd5a04446aaeca554e929473c
17TheWord <17theword@gmail.com>
2024-09-10 12:00:54 +0800

    ✅ Add event test

---
+++ tests/test_event.py
@@ -0,0 +1,15 @@
+import json
+import pytest
+from pathlib import Path
+
+from nonebot.compat import type_validate_python
+from nonebot.adapters.afdian.event import OrderNotifyEvent
+
+
+@pytest.mark.asyncio
+async def test_event():
+    with (Path(__file__).parent / "events.json").open("r") as f:
+        test_event = json.load(f)
+
+    parsed = type_validate_python(OrderNotifyEvent, test_event)
+    assert isinstance(parsed, OrderNotifyEvent)
