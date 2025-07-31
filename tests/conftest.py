57226b44f09aff8a3abc15c1c04b00fcc9e19b03
17TheWord <17theword@gmail.com>
2025-07-31 17:52:15 +0800

    :arrow_up: Update dependencies and test configurations

--- tests/conftest.py
+++ tests/conftest.py
@@ -2,27 +2,37 @@ from pathlib import Path
 
 from nonebug import NONEBOT_INIT_KWARGS
 import pytest
+from pytest_asyncio import is_async_test
 
 import nonebot
-from nonebot.adapters.afdian.adapter import Adapter
 
 nonebot.adapters.__path__.append(  # type: ignore
     str((Path(__file__).parent.parent / "nonebot" / "adapters").resolve())
 )
 
+from nonebot.adapters.afdian import Adapter  # type: ignore
+
 
 def pytest_configure(config: pytest.Config) -> None:
     config.stash[NONEBOT_INIT_KWARGS] = {
+        "driver": "~fastapi+~httpx",
         "afdian_bots": [
             {
                 "user_id": "user_id1",
                 "api_token": "api_token1",
             },
-        ]
+        ],
     }
 
 
+def pytest_collection_modifyitems(items: list[pytest.Item]):
+    pytest_asyncio_tests = (item for item in items if is_async_test(item))
+    session_scope_marker = pytest.mark.asyncio(loop_scope="session")
+    for async_test in pytest_asyncio_tests:
+        async_test.add_marker(session_scope_marker, append=False)
+
+
 @pytest.fixture(scope="session", autouse=True)
-def _init_adapter(nonebug_init: None):
+def after_nonebot_init(after_nonebot_init: None):
     driver = nonebot.get_driver()
     driver.register_adapter(Adapter)
--- tests/test_config.py
+++ tests/test_config.py
@@ -1,10 +1,10 @@
 from nonebot import get_adapter
-from nonebot.adapters.afdian import Adapter
-from nonebot.adapters.afdian.config import BotInfo
+from nonebot.adapters.afdian import Adapter  # type: ignore
+from nonebot.adapters.afdian.config import BotInfo  # type: ignore
 
 
 def test_config():
     adapter = get_adapter(Adapter)
-    config = adapter.afdian_config
+    config = adapter.afdian_config  # type: ignore
 
     assert config.afdian_bots == [BotInfo(api_token="api_token1", user_id="user_id1")]
