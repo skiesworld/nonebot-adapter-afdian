4e6970ab0da17a55ccf60188f707110d11278864
17TheWord <17theword@gmail.com>
2025-09-11 20:24:41 +0800

    `:wrench: Remove afdian_enable_webhook_bot config option`

--- nonebot/adapters/afdian/config.py
+++ nonebot/adapters/afdian/config.py
@@ -10,4 +10,3 @@ class Config(BaseModel):
     afdian_bots: list[BotInfo] = Field(default_factory=list)
     afdian_api_base: str = Field("https://afdian.com")
     afdian_hook_secret: str = Field("")
-    afdian_enable_webhook_bot: bool = Field(False)
