7a0a7594b1224064f236e5f7c7b4fc95b815a8e6
17TheWord <17theword@gmail.com>
2025-09-29 17:10:20 +0800

    🐛 Refactor error handling and response parsing in Afdian adapter

--- nonebot/adapters/afdian/bot.py
+++ nonebot/adapters/afdian/bot.py
@@ -6,7 +6,7 @@ from nonebot.message import handle_event
 
 from .event import Event
 from .message import Message, MessageSegment
-from .payload import OrderResponse, PingResponse, SponsorResponse, WrongResponse
+from .payload import OrderResponse, PingResponse, SponsorResponse
 from .utils import construct_request, parse_response
 
 if TYPE_CHECKING:
@@ -41,7 +41,7 @@ class TokenBot(HookBot):
         super().__init__(adapter, self_id)
         self.token = token
 
-    async def send_ping(self) -> PingResponse | WrongResponse:
+    async def send_ping(self) -> PingResponse:
         request = construct_request(
             self.adapter.afdian_config.afdian_api_base + "/api/open/ping",
             self.self_id,
@@ -51,9 +51,7 @@ class TokenBot(HookBot):
         response = await self.adapter.request(request)
         return parse_response(response, PingResponse)
 
-    async def __query_order(
-        self, params: dict[str, Any]
-    ) -> OrderResponse | WrongResponse:
+    async def __query_order(self, params: dict[str, Any]) -> OrderResponse:
         request = construct_request(
             self.adapter.afdian_config.afdian_api_base + "/api/open/query-order",
             self.self_id,
@@ -63,24 +61,22 @@ class TokenBot(HookBot):
         response = await self.adapter.request(request)
         return parse_response(response, OrderResponse)
 
-    async def query_order_by_page(self, page: int):
+    async def query_order_by_page(self, page: int) -> OrderResponse:
         """根据页码查询订单"""
         if page <= 0:
             raise ValueError("page must be greater than 0")
         return await self.__query_order(params={"page": page})
 
-    async def query_order_by_out_trade_no(self, out_trade_no: str):
+    async def query_order_by_out_trade_no(self, out_trade_no: str) -> OrderResponse:
         """根据订单号查询订单"""
         return await self.__query_order(params={"out_trade_no": out_trade_no})
 
-    async def query_order_by_order_list(self, order_list: list[str]):
+    async def query_order_by_order_list(self, order_list: list[str]) -> OrderResponse:
         """根据订单号列表查询多个订单"""
         order_list_str = ",".join(order_list)
         return await self.__query_order(params={"out_trade_no": order_list_str})
 
-    async def query_sponsor(
-        self, page: int, per_page: int = 20
-    ) -> SponsorResponse | WrongResponse:
+    async def query_sponsor(self, page: int, per_page: int = 20) -> SponsorResponse:
         """查询赞助者，可选传参每页数量 1-100"""
         if page <= 0:
             raise ValueError("page must be greater than 0")
